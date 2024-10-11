9ff12aa40e86c5b7e3c809f3cbadc530b06b72eb
Sebastian Diaz <diazebas@gmail.com>
2024-10-11 17:37:43 +0000

    Flask API Python Assignment

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Global variable to hold todos
 todos = [
     { "label": "My first task", "done": False },
     { "label": "My second task", "done": False }
@@ -39,6 +38,6 @@ def delete_todo(position):
     # Return the updated todos list in JSON format
     return jsonify(todos), 200  # Return 200 OK
 
-# These two lines should always be at the end of your app.py file
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
